0aa988534eddcfe264a138a253ef0335b6018f1c
Zewsic <66947765+Zewsic@users.noreply.github.com>
2021-06-16 13:45:47 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1623840439,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/gun_1-sheet0.png",
+		"images/bullet_1-sheet0.png",
+		"images/enemy_1-sheet0.png",
+		"images/arrow_blast-sheet0.png",
+		"images/fireer-sheet0.png",
+		"images/saw-sheet0.png",
+		"images/fire.png",
+		"images/piner-sheet0.png",
+		"images/deleter.png",
+		"images/finish.png",
+		"images/colluser.png",
+		"images/stars-sheet0.png",
+		"images/player-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
